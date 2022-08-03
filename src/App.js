4d91ad863cb848708d253d0ca2e0d130160e8bd6
Michael Okello <okellofrost@gmail.com>
2022-08-03 14:23:17 +0300

    Routing switch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from "react-router-dom";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
+import { Switch } from "react-router-dom";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
@@ -15,10 +16,12 @@ class App extends Component {
       
       <div >
         <div className="content">
+          <Switch>
           <Route path="/products" component={Products}/>
           <Route path="/posts" component={Posts}/>
           <Route path="/admin" component={Dashboard}/>
           <Route path="/" exact component={Home}/>
+          </Switch>
           </div>
         <NavBar />
       </div>
